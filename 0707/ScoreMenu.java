0f87d9ea36dfa265275505fc4f4c0ae65b0ce1b8
unknown <alan1070012@gmail.com>
2026-07-07 13:42:13 +0800

    Add 0707 score menu homework

---
+++ 0707/ScoreMenu.java
@@ -0,0 +1,52 @@
+import java.util.*;
+public class ScoreMenu {
+    public static void main(String[] argv){
+        System.out.println("請輸入姓名");
+        System.out.println("請輸入 Java、English、Math 三科成績");
+
+        Scanner sc = new Scanner(System.in);
+        String name = sc.next();
+        Double Java = sc.nextDouble();
+        Double English = sc.nextDouble();
+        Double Math = sc.nextDouble();
+        System.out.println("請輸入 0,1,2,3");
+        int option = sc.nextInt();
+        while(option>0) {
+
+        switch(option){
+            case 0:
+                System.out.println("離開");
+                break;
+            case 1:
+                System.out.println("平均分數:"+(Java+English+Math)/3);
+                break;
+            case 2:
+                if((Java+English+Math)/3 >=60){
+                    System.out.println("及格");
+                }else{
+                    System.out.println("不及格");
+                }  
+                break; 
+            case 3:
+                if((Java+English+Math)/3<60){
+                    System.out.println("F");
+                }else if((Java+English+Math)/3>=60&&(Java+English+Math)/3<70){
+                    System.out.println("D");
+                }else if((Java+English+Math)/3>=70&&(Java+English+Math)/3<80){
+                    System.out.println("C");
+                }else if((Java+English+Math)/3>=80&&(Java+English+Math)/3<90){
+                    System.out.println("B");
+                }else if((Java+English+Math)/3>=90&&(Java+English+Math)/3<100){
+                    System.out.println("A");
+                }
+                break;
+            default:      
+                System.out.println("Unknown option");
+            }sc.close();    
+        }
+        
+
+
+    }
+    
+}
